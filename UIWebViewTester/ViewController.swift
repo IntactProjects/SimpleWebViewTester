ecc7219e19e52dfd6608de65e9677ea446c0a92a
OBayle <olivier.bayle@in-tact.fr>
2018-02-01 15:00:12 +0100

    add: webview and constraints

--- UIWebViewTester/ViewController.swift
+++ UIWebViewTester/ViewController.swift
@@ -1,18 +1,14 @@
-//
-//  ViewController.swift
-//  UIWebViewTester
-//
-//  Created by olivier on 23/01/2018.
-//  Copyright © 2018 in-Tact. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL (string: "http://91.183.88.43:8204/V1/Sjr")
+        let requestObj = URLRequest(url: url!)
+        webView.loadRequest(requestObj)
     }
 
     override func didReceiveMemoryWarning() {
